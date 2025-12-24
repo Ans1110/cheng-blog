dcd8a8248fd65287192f4cb9723eccd7fee9e90f
Xiaode Zheng <your.email@example.com>
2025-12-25 02:12:09 +0800

    feat:  add Providers component for theme and query management

---
+++ components/Providers.tsx
@@ -0,0 +1,40 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+import { toast, Toaster } from "sonner";
+
+interface ProvidersProps {
+  children: React.ReactNode;
+}
+
+const Providers = ({ children }: ProvidersProps) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      mutations: {
+        onError: (err) => {
+          if (err.message === "NEXT_REDIRECT") return;
+          toast.error(err.message);
+        },
+        onSuccess: () => {
+          toast.success("Operation successful");
+        },
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Toaster />
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default Providers;
+export { Providers };
