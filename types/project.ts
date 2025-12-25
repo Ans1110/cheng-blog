1eabfb60c84134f54c68f063b99d5e5b0a42daf8
Xiaode Zheng <your.email@example.com>
2025-12-25 13:14:39 +0800

    feat: Add CreateProject and UpdateProject interfaces

--- types/project.ts
+++ types/project.ts
@@ -9,3 +9,21 @@ export interface Project {
   createdAt: Date | string;
   updatedAt: Date | string;
 }
+
+export interface CreateProject {
+  title: string;
+  description: string;
+  tags: string[];
+  imageUrl?: string | null;
+  projectUrl?: string | null;
+  githubUrl?: string | null;
+}
+
+export interface UpdateProject {
+  title?: string;
+  description?: string;
+  tags?: string[];
+  imageUrl?: string | null;
+  projectUrl?: string | null;
+  githubUrl?: string | null;
+}
